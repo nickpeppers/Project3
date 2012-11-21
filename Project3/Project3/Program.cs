351780117961598ad917fdf0159d04f3982d7f6d
Nick Peppers <nick.peppers@hitcents.com>
2012-11-21 15:40:11 -0600

    Started Project 3

---
+++ Project3/Project3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project3
+{
+    class Program
+    {
+        private int[] buffer = new int[20];
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void Producer()
+        {
+
+        }
+
+        public void Consumer()
+        {
+
+        }
+    }
+}
