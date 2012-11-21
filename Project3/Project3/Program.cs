6d171931222363c0d89e5a7e8f055513a4659bdf
Nick Peppers <nick.peppers@hitcents.com>
2012-11-21 15:54:52 -0600

    Added to project
    Need to add a queue instead of a stack

--- Project3/Project3/Program.cs
+++ Project3/Project3/Program.cs
@@ -2,26 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Project3
 {
     class Program
     {
-        private int[] buffer = new int[20];
+        private Stack<int> stack = new Stack<int>();
+        private int count = 0;
+        private object locker = new object();
 
         static void Main(string[] args)
         {
 
         }
 
+        public void Push(int x)
+        {
+            lock (locker)
+            {
+                stack.Push(x);
+            }
+        }
+
+        public int Pop()
+        {
+            lock (locker)
+            {
+                if (stack.Count == 0)
+                {
+                    return -1;
+                }
+
+                return stack.Pop();
+            }
+        }
+
         public void Producer()
         {
+            while (true)
+            {
+                Thread.Sleep(200);
 
+                Push(++count);
+                Console.WriteLine("Producer: " + count);
+            }
         }
 
         public void Consumer()
         {
+            while (true)
+            {
+                Thread.Sleep(100);
 
+                Console.WriteLine("Consumer: " + Pop());
+            }
         }
     }
 }
